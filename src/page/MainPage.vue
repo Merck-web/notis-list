e9cc078cc173d652f4ca6bc14c6277cc73c32d12
-Miracle <73531883+Merck-web@users.noreply.github.com>
2024-11-24 10:21:43 +0500

    init + главная

---
+++ src/page/MainPage.vue
@@ -0,0 +1,50 @@
+<template>
+    <div
+        class="main-page container"
+    >
+        <header class="row items-center justify-between">
+            <img
+                src="/icons/logo.svg"
+                alt="Логотип"
+            />
+            
+            <button class="button-default">
+                <img
+                    src="/icons/sign.svg"
+                    alt="Иконка войти"
+                />
+                
+                Вход
+            </button>
+        </header>
+        
+        
+        <div class="main-page__content">
+            <div class="main-page__content--text">
+                <h1>
+                    Мои <br> заметки
+                </h1>
+                
+                <h3 class="text-gray">
+                    Не забывай о важном, <br> храни его в облаке.
+                </h3>
+            </div>
+            
+            <img
+                class="main-page__content--img"
+                src="/images/main-img.png"
+                alt="Главная картинка"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainPage',
+};
+</script>
+
+<style scoped>
+
+</style>
